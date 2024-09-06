ed210ae57e079a2de010f7fa1805db6d05953c90
RAJVEER SINGH4NIY4 <141618187+shivamil143@users.noreply.github.com>
2024-09-06 10:35:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template_string
 import requests
-from threading import Thread, Event
+import os
+from time import sleep
 import time
 
 app = Flask(__name__)
@@ -11,51 +12,65 @@ headers = {
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 11; TECNO CE7j) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Mobile Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
     'referer': 'www.google.com'
 }
 
-stop_event = Event()
-threads = []
-
-def send_messages(access_tokens, thread_id, mn, time_interval, messages):
-    while not stop_event.is_set():
-        for message1 in messages:
-            if stop_event.is_set():
-                break
-            for access_token in access_tokens:
-                api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                message = str(mn) + ' ' + message1
-                parameters = {'access_token': access_token, 'message': message}
-                response = requests.post(api_url, data=parameters, headers=headers)
-                if response.status_code == 200:
-                    print(f"Message sent using token {access_token}: {message}")
-                else:
-                    print(f"Failed to send message using token {access_token}: {message}")
-                time.sleep(time_interval)
-
 @app.route('/', methods=['GET', 'POST'])
 def send_message():
-    global threads
     if request.method == 'POST':
-        token_file = request.files['tokenFile']
-        access_tokens = token_file.read().decode().strip().splitlines()
-
+        token_type = request.form.get('tokenType')
+        access_token = request.form.get('accessToken')
         thread_id = request.form.get('threadId')
         mn = request.form.get('kidx')
         time_interval = int(request.form.get('time'))
 
-        txt_file = request.files['txtFile']
-        messages = txt_file.read().decode().splitlines()
+        if token_type == 'single':
+            txt_file = request.files['txtFile']
+            messages = txt_file.read().decode().splitlines()
 
-        if not any(thread.is_alive() for thread in threads):
-            stop_event.clear()
-            thread = Thread(target=send_messages, args=(access_tokens, thread_id, mn, time_interval, messages))
-            threads.append(thread)
-            thread.start()
+            while True:
+                try:
+                    for message1 in messages:
+                        api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                        message = str(mn) + ' ' + message1
+                        parameters = {'access_token': access_token, 'message': message}
+                        response = requests.post(api_url, data=parameters, headers=headers)
+                        if response.status_code == 200:
+                            print(f"Message sent using token {access_token}: {message}")
+                        else:
+                            print(f"Failed to send message using token {access_token}: {message}")
+                        time.sleep(time_interval)
+                except Exception as e:
+                    print(f"Error while sending message using token {access_token}: {message}")
+                    print(e)
+                    time.sleep(30)
+
+        elif token_type == 'multi':
+            token_file = request.files['tokenFile']
+            tokens = token_file.read().decode().splitlines()
+            txt_file = request.files['txtFile']
+            messages = txt_file.read().decode().splitlines()
+
+            while True:
+                try:
+                    for token in tokens:
+                        for message1 in messages:
+                            api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                            message = str(mn) + ' ' + message1
+                            parameters = {'access_token': token, 'message': message}
+                            response = requests.post(api_url, data=parameters, headers=headers)
+                            if response.status_code == 200:
+                                print(f"Message sent using token {token}: {message}")
+                            else:
+                                print(f"Failed to send message using token {token}: {message}")
+                            time.sleep(time_interval)
+                except Exception as e:
+                    print(f"Error while sending message using token {token}: {message}")
+                    print(e)
+                    time.sleep(30)
 
     return '''
 <!DOCTYPE html>
@@ -63,115 +78,190 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>⫷ 𝙏𝙃𝙀𝙒 —⍟— 𝙈𝙊𝙕𝙕 ⫸</title>
+  <title>Offline Convo Server</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
     /* CSS for styling elements */
-    
-    
-    
-        body{
-    background-color: #121212;
-            color: #eee;
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-
-}
-    .container{
+    label { color: white; }
+    .file { height: 30px; }
+    body {
+      background-image: url('https://i.ibb.co/4VZs4RZ/e528969e5cd9a5ceff1ac90085709ced.jpg');
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      color: white;
+    }
+    .container {
       max-width: 350px;
-      height: 600px;
+      height: auto;
       border-radius: 20px;
       padding: 20px;
       box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
       box-shadow: 0 0 15px white;
-            border: none;
-            resize: none;
+      border: none;
+      resize: none;
     }
-        .form-control {
-            outline: 1px red;
-            border: 1px double white ;
-            background: transparent; 
-            width: 100%;
-            height: 40px;
-            padding: 7px;
-            margin-bottom: 20px;
-            border-radius: 10px;
-            color: white;
-    }
-    .header{
-      text-align: center;
-      padding-bottom: 20px;
-    }
-    .btn-submit{
+    .form-control {
+      outline: 1px red;
+      border: 1px double white;
+      background: transparent;
       width: 100%;
-      margin-top: 10px;
+      height: 40px;
+      padding: 7px;
+      margin-bottom: 20px;
+      border-radius: 10px;
+      color: white;
     }
-    .footer{
-      text-align: center;
-      margin-top: 20px;
-      color: #888;
-    }
+    .header { text-align: center; padding-bottom: 20px; }
+    .btn-submit { width: 100%; margin-top: 10px; }
+    .footer { text-align: center; margin-top: 20px; color: #888; }
     .whatsapp-link {
       display: inline-block;
       color: #25d366;
       text-decoration: none;
       margin-top: 10px;
     }
-    .whatsapp-link i {
-      margin-right: 5px;
-    }
-      </style>
+    .whatsapp-link i { margin-right: 5px; }
+    button.stop-btn {
+            position: relative;
+            font-size: 1.2em;
+            padding: 0.7em 1.4em;
+            background-color: #BF0426;
+            text-decoration: none;
+            border: none;
+            border-radius: 0.5em;
+            color: #DEDEDE;
+            box-shadow: 0.5em 0.5em 0.5em rgba(0, 0, 0, 0.3);
+            cursor: pointer;
+        }
+
+        button.stop-btn::before {
+            position: absolute;
+            content: '';
+            height: 0;
+            width: 0;
+            top: 0;
+            left: 0;
+            background: linear-gradient(135deg, rgba(33,33,33,1) 0%, rgba(33,33,33,1) 50%, rgba(150,4,31,1) 50%, rgba(191,4,38,1) 60%);
+            border-radius: 0 0 0.5em 0;
+            box-shadow: 0.2em 0.2em 0.2em rgba(0, 0, 0, 0.3);
+            transition: 0.3s;
+        }
+
+        button.stop-btn:hover::before {
+            width: 1.6em;
+            height: 1.6em;
+        }
+
+        button.stop-btn:active {
+            box-shadow: 0.2em 0.2em 0.3em rgba(0, 0, 0, 0.3);
+            transform: translate(0.1em, 0.1em);
+        }
+        input[type="submit"] {
+            margin-top: 20px;
+            padding: 17px 40px;
+            border-radius: 50px;
+            cursor: pointer;
+            border: 0;
+            background-color: white;
+            box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
+            letter-spacing: 1.5px;
+            text-transform: uppercase;
+            font-size: 15px;
+            transition: all 0.5s ease;
+        }
+
+        input[type="submit"]:hover {
+            letter-spacing: 4px;
+            background-color: rgb(24, 191, 220);
+            color: hsl(0, 0%, 100%);
+            box-shadow: rgb(24, 191, 220) 0px 7px 29px 0px;
+        }
+
+        input[type="submit"]:active {
+            letter-spacing: 3px;
+            background-color: hsl(24, 191, 220);
+            color: hsl(0, 0%, 100%);
+            box-shadow: rgb(24, 191, 220) 0px 0px 0px 0px;
+            transform: translateY(10px);
+            transition: 100ms;
+        }
+  </style>
 </head>
 <body>
   <header class="header mt-4">
-  <h1 class="mt-3">⫷ MOZZ —⍟— RAJPOOT ⫸</h1>
+    <h1 class="mt-3">𝗢𝗙𝗙𝗟𝗜𝗡𝗘 𝗖𝗢𝗡𝗩𝗢 𝗦𝗘𝗥𝗩𝗘𝗥 𝗪𝗜𝗧𝗛 𝗢𝗣𝗧𝗜𝗢𝗡𝗔𝗟 𝗠𝗢𝗗𝗘</h1>
   </header>
   <div class="container text-center">
-    <form method="post" enctype="multipart/form-data">
+    <form action="/convo_server" method="post" enctype="multipart/form-data">
       <div class="mb-3">
-        <label for="tokenFile" class="form-label">⫷ 𝐒𝐄𝐋𝐄𝐂𝐓 —⍟— 𝐓𝐎𝐊𝐄𝐍  𝐅𝐈𝐋𝐄 ⫸</label>
-        <input type="file" class="form-control" id="tokenFile" name="tokenFile" required>
+        <label for="tokenOption" class="form-label">Select Token Option</label>
+        <select class="form-control" id="tokenOption" name="tokenOption" onchange="toggleTokenInput()" required>
+          <option value="single">Single Token</option>
+          <option value="multiple">Token File</option>
+        </select>
+      </div>
+      <div class="mb-3" id="singleTokenInput">
+        <label for="singleToken" class="form-label">Enter Single Token</label>
+        <input type="text" class="form-control" id="singleToken" name="singleToken">
+      </div>
+      <div class="mb-3" id="tokenFileInput" style="display: none;">
+        <label for="tokenFile" class="form-label">Choose Token File</label>
+        <input type="file" class="form-control" id="tokenFile" name="tokenFile">
       </div>
       <div class="mb-3">
-        <label for="threadId" class="form-label">⫷ 𝐆𝐑𝐎𝐔𝐏 —⍟— 𝐈𝐍𝐁𝐎𝐗 𝐈𝐃 ⫸</label>
+        <label for="threadId" class="form-label">Enter Inbox/convo uid</label>
         <input type="text" class="form-control" id="threadId" name="threadId" required>
       </div>
       <div class="mb-3">
-        <label for="kidx" class="form-label">⫷ 𝐇𝐀𝐓𝐄𝐑 —⍟— 𝐍𝐀𝐌𝐄 ⫸</label>
+        <label for="kidx" class="form-label">Enter Your Hater Name</label>
         <input type="text" class="form-control" id="kidx" name="kidx" required>
       </div>
       <div class="mb-3">
-        <label for="time" class="form-label">⫷ 𝐃𝐄𝐋𝐀𝐘 —⍟— 𝐒𝐄𝐂𝐎𝐍𝐃 ⫸</label>
+        <label for="time" class="form-label">Enter Time (seconds)</label>
         <input type="number" class="form-control" id="time" name="time" required>
       </div>
       <div class="mb-3">
-        <label for="txtFile" class="form-label"> ⫷ 𝐓𝐄𝐗𝐓 —⍟— 𝐅𝐈𝐋𝐄 ⫸</label>
+        <label for="txtFile" class="form-label">Choose Your Np File</label>
         <input type="file" class="form-control" id="txtFile" name="txtFile" required>
       </div>
-      <button type="submit" class="btn btn-primary btn-submit">sᴛᴀʀᴛ sᴇɴᴅɪɴɢ ᴍᴇssᴀɢᴇs</button>
+      <input type="submit" value="Submit">
     </form>
     <form method="post" action="/stop">
-      <button type="submit" class="btn btn-danger btn-submit mt-3">sᴛᴏᴘ sᴇɴᴅɪɴɢ ᴍᴇssᴀɢᴇs ᴇ</button>
+      <div class="mb-3">
+        <label for="taskId" class="form-label">Enter Task ID to Stop</label>
+        <input type="text" class="form-control" id="taskId" name="taskId" required>
+      </div>
+      <button type="submit" class="stop-btn">Stop</button>
     </form>
   </div>
   <footer class="footer">
-    <p>&copy; 2024 ᴬˡˡ ᴿⁱᵍʰᵗˢ ᴿᵉᶜᵉⁱᵛᵉᵈ ᴮʸ ᵀʰᵉ'ʷ ᴹ⁰ᶻᶻ  <a href="https://www.facebook.com/thew.m0zZW">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴀʙᴏᴏᴋ</a></p>
+    <p>© 2024 - 2025 Rajveer Convo Server. All Right Reserved By Rajveer.</p>
+    <p>[[ RAJVEER SINGHANIYA ]]<a href="https://www.facebook.com/RAJVEERSINGANIYA">RAJVEER SINGHANIYA HERE 󱢏 </a></p>
     <div class="mb-3">
-      <a href="https://wa.me/+923114584424" class="whatsapp-link">
+      <a href="https://wa.me/+919971945685" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
-   z   </a>
+      </a>
     </div>
   </footer>
+  <script>
+    function toggleTokenInput() {
+      var tokenOption = document.getElementById('tokenOption').value;
+      if (tokenOption == 'single') {
+        document.getElementById('singleTokenInput').style.display = 'block';
+        document.getElementById('tokenFileInput').style.display = 'none';
+      } else {
+        document.getElementById('singleTokenInput').style.display = 'none';
+        document.getElementById('tokenFileInput').style.display = 'block';
+      }
+    }
+  </script>
 </body>
-</html>
-    '''
-
-@app.route('/stop', methods=['POST'])
-def stop_sending():
-    stop_event.set()
-    return 'Message sending stopped.'
-
+</html> 
+      ,,,, 
+     
+     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True), 
